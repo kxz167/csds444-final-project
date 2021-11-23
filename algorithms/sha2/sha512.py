8f2307ddcbef1b423a4c8081fd873bfb7293dd11
Kris Zhao <kriszhao@comcast.net>
2021-11-23 08:14:30 -0500

    Cleanup

--- algorithms/sha2/sha512.py
+++ algorithms/sha2/sha512.py
@@ -23,55 +23,168 @@
 #output
 
 input_file_path = "wiki"
-
 byte_block = 128 # 1024 bits
+bit_length = 2**64
 
-# DETERMINE SIZE:
+# CONSTANTS:
+word_constants=[0x428a2f98d728ae22,0x7137449123ef65cd,0xb5c0fbcfec4d3b2f,0xe9b5dba58189dbbc,0x3956c25bf348b538,0x59f111f1b605d019,0x923f82a4af194f9b,0xab1c5ed5da6d8118,0xd807aa98a3030242,0x12835b0145706fbe,0x243185be4ee4b28c,0x550c7dc3d5ffb4e2,0x72be5d74f27b896f,0x80deb1fe3b1696b1,0x9bdc06a725c71235,0xc19bf174cf692694,0xe49b69c19ef14ad2,0xefbe4786384f25e3,0x0fc19dc68b8cd5b5,0x240ca1cc77ac9c65,0x2de92c6f592b0275,0x4a7484aa6ea6e483,0x5cb0a9dcbd41fbd4,0x76f988da831153b5,0x983e5152ee66dfab,0xa831c66d2db43210,0xb00327c898fb213f,0xbf597fc7beef0ee4,0xc6e00bf33da88fc2,0xd5a79147930aa725,0x06ca6351e003826f,0x142929670a0e6e70,0x27b70a8546d22ffc,0x2e1b21385c26c926,0x4d2c6dfc5ac42aed,0x53380d139d95b3df,0x650a73548baf63de,0x766a0abb3c77b2a8,0x81c2c92e47edaee6,0x92722c851482353b,0xa2bfe8a14cf10364,0xa81a664bbc423001,0xc24b8b70d0f89791,0xc76c51a30654be30,0xd192e819d6ef5218,0xd69906245565a910,0xf40e35855771202a,0x106aa07032bbd1b8,0x19a4c116b8d2d0c8,0x1e376c085141ab53,0x2748774cdf8eeb99,0x34b0bcb5e19b48a8,0x391c0cb3c5c95a63,0x4ed8aa4ae3418acb,0x5b9cca4f7763e373,0x682e6ff3d6b2b8a3,0x748f82ee5defb2fc,0x78a5636f43172f60,0x84c87814a1f0ab72,0x8cc702081a6439ec,0x90befffa23631e28,0xa4506cebde82bde9,0xbef9a3f7b2c67915,0xc67178f2e372532b,0xca273eceea26619c,0xd186b8c721c0c207,0xeada7dd6cde0eb1e,0xf57d4f7fee6ed178,0x06f067aa72176fba,0x0a637dc5a2c898a6,0x113f9804bef90dae,0x1b710b35131c471b,0x28db77f523047d84,0x32caab7b40c72493,0x3c9ebe0a15c9bebc,0x431d67c49c100d4c,0x4cc5d4becb3e42b6,0x597f299cfc657e2a,0x5fcb6fab3ad6faec,0x6c44198c4a475817]
+initial_vector_values = [0x6a09e667f3bcc908,0xbb67ae8584caa73b,0x3c6ef372fe94f82b,0xa54ff53a5f1d36f1,0x510e527fade682d1,0x9b05688c2b3e6c1f,0x1f83d9abfb41bd6b,0x5be0cd19137e2179]
+print (word_constants)
+print(initial_vector_values)
 
-
-
+# File Size:
+print("==============")
 import os
-
-# 23576 * 1024 = 24 | + 128 = 152. Needs a pad of 872
-
 file_size = os.path.getsize(input_file_path) * 8
 padding = 1024 - ((file_size % 1024 + 128) % 1024)
-
-print("The file size is: ", file_size, "bits.")
-print("That means we need a pad of: ", padding)
-print((padding + file_size + 128) % 1024)
+print("The file size is:", file_size, "bits.")
+print("That means we need a pad of:", padding)
+print("And this then gives us a remainder of:", (padding + file_size + 128) % 1024)
+print("==============")
 
 def sha_pad(value, pad):
+    # print(((value << 1) | 1 ) << (pad-1))
     return ((value << 1) | 1 ) << (pad-1)
 
-# print(sha_pad(5, 5))
+def left_rotate_64(number, shifts):
+    return ((number << shifts) | (number >> (64 - shifts))) & 0xffffffffffffffff
+
+def right_rotate_64(number, shifts):
+    return (number >> shifts) | (number << (64 - shifts)) & 0xffffffffffffffff
+
+def append_64(original, appended):
+    return (original << 64) | appended
+
+def sigma_0 (word):
+    s0 = (right_rotate_64(word, 1, 64) ^ (right_rotate_64(word, 8, 64)) ^ (word >> 7))
+
+
+def sigma_1 (word):
+    s1 = (right_rotate_64(word, 19, 64) ^ (right_rotate_64(word, 61, 64)) ^ (word >> 6))
 
 input_file = open(input_file_path, 'rb')
-# sum = b'0'
 reading = True
+
+# Initial Round Values / RESULTS
+a = initial_vector_values[0]
+b = initial_vector_values[1]
+c = initial_vector_values[2]
+d = initial_vector_values[3]
+e = initial_vector_values[4]
+f = initial_vector_values[5]
+g = initial_vector_values[6]
+h = initial_vector_values[7]
+
+# Initial Hash Value:
+h0 = initial_vector_values[0]
+h1 = initial_vector_values[1]
+h2 = initial_vector_values[2]
+h3 = initial_vector_values[3]
+h4 = initial_vector_values[4]
+h5 = initial_vector_values[5]
+h6 = initial_vector_values[6]
+h7 = initial_vector_values[7]
 
 i=0
 while(reading):
     print("===============")
     i += 1
-    # Read input file as bytes
+    # if(i == 1):
+    #     reading = False
+
+    # Read input file one block at a time
     read = input_file.read(byte_block)
 
-    # If we are at the end:
+    # If we are at the end, pad:
     if(len(read) < byte_block):
-        print(int.from_bytes(read, 'big'))
+        print("Int of bytes read: ", int.from_bytes(read, 'big'))
         
         # Pad with the correct number of bits and then add the file size.
         read_int = (sha_pad(int.from_bytes(read, 'big'), padding) << 128 ) | file_size
+        print("Int after padding + file size: ", read_int)
+        
         # Set th read input back into bits.
-        read = int.to_bytes(read_int, byte_block * 8, 'big')
+        read = int.to_bytes(read_int, byte_block, 'big')
         # End the while loop
         reading = False
     # sum = sum | read
-    print(read)
-    print(len(read))
-    print(int.from_bytes(read, 'big'))
-    # print(sha_pad(int.from_bytes(read, 'big'), 3))
+
+    print("Raw output:",read)
+    print("With length(bytes):",len(read))
+
+    ## DO THE ENCODING HERE ASSUMING READ IS A 1024 BIT BLOCK
+    print("--ENCODING--")
+
+    words = []          # Each generated word
+
+    #GENERATE WORDS
+    for x in range(0,16):
+        # Get certain bits from the read input
+        words.append(int.from_bytes(read[x*8: x*8+8], 'big'))
+
+    for x in range(16, 80):
+        # s0 := (w[i-15] rightrotate 1) xor (w[i-15] rightrotate 8) xor (w[i-15] rightshift 7)
+        s0 = (right_rotate_64(words[x-15], 1) ^ (right_rotate_64(words[x-15], 8)) ^ (words[x-15] >> 7))
+        # print(s0)
+        # s1 := (w[i-2] rightrotate 19) xor (w[i-2] rightrotate 61) xor (w[i-2] rightshift 6)
+        s1 = (right_rotate_64(words[x-2], 19) ^ (right_rotate_64(words[x-2], 61)) ^ (words[x-2] >> 6))
+        # print(s1)
+        # w[i] := w[i-16] + s0 + w[i-7] + s1
+        words.append((words[x-16] + s0 + words[x-7] + s1) % (bit_length))
+
+    print("Message Schedule:", words)
+    print(len(words))
+    # for i from 0 to 63
+    #PERFORM ROUNDS: 
+    for x in range(0, 80):
+        # S1 := (e rightrotate 14) xor (e rightrotate 18) xor (e rightrotate 41)
+        S1 = (right_rotate_64(e, 14) ^ (right_rotate_64(e, 18)) ^ (right_rotate_64(e, 41)))
+        # ch := (e and f) xor ((not e) and g)
+        ch = (e & f) ^ ((~e) & g) 
+        # temp1 := h + S1 + ch + k[i] + w[i]
+        temp1 = (h + S1 + ch + word_constants[x] + words[x]) % bit_length
+        # S0 := (a rightrotate 28) xor (a rightrotate 34) xor (a rightrotate 39)
+        S0 = (right_rotate_64(a, 28) ^ (right_rotate_64(a, 34) ^ (right_rotate_64(a, 38))))
+        # maj := (a and b) xor (a and c) xor (b and c)
+        maj = (a & b) ^ (a & c) ^ (b & c)
+        # temp2 := S0 + maj
+        temp2 = (S0 + maj)% bit_length
+
+        # T1 = h + ch(e, f, g) sigma(e) + words[x] + word_constants[x]
+ 
+        # h := g
+        h = g
+        # g := f
+        g = f
+        # f := e
+        f = e
+        # e := d + temp1
+        e = (d + temp1) % bit_length
+        # d := c
+        d = c
+        # c := b
+        c = b
+        # b := a
+        b = a
+        # a := temp1 + temp2
+        a = (temp1 + temp2) % bit_length
+
+    # Add the compressed chunk to the current hash value:
+    h0 = (h0 + a) % bit_length
+    h2 = (h2 + b) % bit_length
+    h3 = (h3 + c) % bit_length
+    h4 = (h4 + d) % bit_length
+    h1 = (h1 + e) % bit_length
+    h5 = (h5 + f) % bit_length
+    h6 = (h6 + g) % bit_length
+    h7 = (h7 + h) % bit_length
+    print("Hashes:", h0, h1, h2, h3, h4, h5, h6, h7)
+    print("===============")
+    
+print("Final Results:")
+print(append_64(append_64(append_64(append_64(append_64(append_64(append_64(h0, h1), h2), h3), h4), h5), h6), h7))
+print("With: ", i, " Rounds")
+# print(right_rotate(864691128455136524, 2, 64))
 
 # print(sum)
 
