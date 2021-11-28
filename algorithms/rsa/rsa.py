126d2b101be9cf1fa2253758ac49020f0026ee6d
Stamatis-P <75396346+Stamatis-P@users.noreply.github.com>
2021-11-27 23:13:45 -0500

    still not fixed, more attempts made

--- algorithms/rsa/rsa.py
+++ algorithms/rsa/rsa.py
@@ -4,7 +4,7 @@ import util
 
 class RSA:
 
-    def __init__(self, size=5):
+    def __init__(self, size=20):
         self.bit_size = size
 
         self.p = util.generate_prime(self.bit_size)
@@ -26,28 +26,32 @@ class RSA:
 
         d = util.mult_inverse(e, self.n)
 
-        print("Public and private keys:", ((e, self.n), (d, self.n)))
-
         public_key = (e, self.n)
         private_key = (d, self.n)
 
         return public_key, private_key
 
-    def encrypt(self, m, key):
-        m = util.string_to_long(m)
+    def encrypt(self, m: int, key):
         e, n = key
         c = util.mod_exp(m, e, n)
-
         return c
 
-
-    def decrypt(self, c):
+    def decrypt(self, c: int):
         d, n = self.private_key
         p = util.mod_exp(c, d, n)
-        #ok now how to get a string genius
         return p
 
+
 if __name__ == '__main__':
+
     rsa = RSA()
-    c = RSA.encrypt(rsa, "hello world", rsa.private_key)
-    RSA.decrypt(rsa, c)
+    m = util.string_to_long("hello world")
+    c = RSA.encrypt(rsa, m, rsa.public_key)
+    p = RSA.decrypt(rsa, c)
+    print("Message: hello world")
+    print("Public key:", rsa.public_key)
+    print("Private key:", rsa.private_key)
+    print("Message int:", m)
+    print("Ciphertext:", c)
+    print("Plaintext int:", p)
+    print("Decrypted plaintext:", util.long_to_bytes(p))
