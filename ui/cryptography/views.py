c71580b5b7dfd865568c1d94ad88180e0925257a
Kris Zhao <kriszhao@comcast.net>
2021-11-29 20:39:36 -0500

    cleared old code

--- ui/cryptography/views.py
+++ ui/cryptography/views.py
@@ -38,76 +38,12 @@ TEXT_ALGO = {
 }
 
 
-# class EncryptForm (forms.Form):
-#     title = forms.CharField(max_length = 50)
-#     algo = forms.MultipleChoiceField(choices = ENCRYPTION_TYPES)
-#     method = forms.MultipleChoiceField(choices = INPUT_TYPES)
-#     file = forms.FileField()
-
-# class EncryptOptionForm (forms.Form):
-#     show_step = forms.BooleanField()
-#     show_step_description = forms.BooleanField()
-#     pause_on_step = forms.BooleanField()
-
-# class TextEncryptForm (forms.Form):
-#     body = forms.CharField(widget=forms.Textarea, label="body",required=True)
-
-# class ResultForm (forms.Form):
-#     steps = forms.JSONField()
-#     step_index = forms.IntegerField()
-#     args = forms.JSONField()
-
-
-
 # Create your views here.
 def index(request):
-    # template = loader.get_template("cryptography/index.html")
-    # context = {
-    #     "my_value": 1
-    # }
-    # return HttpResponse(template.render(context, request))
-
-    # Alternatively:
-    # context = {
-    #     "my_value": 1
-    # }
-    # return render(request, 'cryptography/index.html', context)
-
-    # Forms:
-    # if request.method == 'POST':
-    #     form = EncryptForm(request.POST, request.FILES)
-    #     if form.is_valid():
-    #         handle_uploaded_file(request.FILES['file'])
-    #         return HttpResponseRedirect('/success/url/')
-    # else:
-    #     form = EncryptForm()
     return render(request, 'cryptography/index.html', {})
 
-# From django
-# def upload_file(request):
-#     if request.method == 'POST':
-#         form = UploadFileForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             handle_uploaded_file(request.FILES['file'])
-#             return HttpResponseRedirect('/success/url/')
-#     else:
-#         form = UploadFileForm()
-#     return render(request, 'upload.html', {'form': form})
 
 def text(request: WSGIRequest):
-    # if request.method == 'POST':
-    #     form = EncryptOptionForm(request.POST, request.FILES)
-    #     if form.is_valid():
-    #         args = request.POST
-    #         return render(request, 'cryptography/sha.html', {
-    #                 'args': args, 
-    #                 # Assuming, we are using on sha only
-    #                 'steps': sha_visual(bytes(args['plaintext'], 'utf-8'))
-    #             }
-    #         )
-    # else:
-    # enc_opt_form = EncryptOptionForm()
-    # text_enc_form = TextEncryptForm()
     return render(request, 'cryptography/text.html', {})
 
 def file(request):
@@ -135,13 +71,6 @@ def result(request: WSGIRequest):
                 destination.write(chunk)
         
         parsed_steps = FILE_ALGO[algorithm]('uploads/plain_file') # Must load algorithms
-
-    print(algorithm)
-    print(showstep)
-    print(type(showstep))
-
-    print(args)
-    print(files)
 
     return render(request, 'cryptography/results.html', {
             # 'resultForm': results,
