645d9923b7790e1e1c4aa60f05e436ffec365f90
Phan Trinh <phan181299@gmail.com>
2021-12-01 00:50:50 -0500

    Fix for results on files

--- ui/cryptography/visuals/visuals.py
+++ ui/cryptography/visuals/visuals.py
@@ -21,7 +21,7 @@ def sha256_visual(msg: str, is_file=False, showstep=False):
     with open(temp_file_path, 'w') as temp_file:
         temp_file.write(sha.hexdigest())
 
-    return {'string_text': sha.hexdigest(), 'files': {'Hash File': temp_file_path}}, sha.steps
+    return {'string_text': sha.hexdigest(), 'file': {'Hash File': temp_file_path}}, sha.steps
 
 def sha512_visual(msg: str, is_file=False, showstep=False):
     bytes_msg = None
@@ -36,7 +36,7 @@ def sha512_visual(msg: str, is_file=False, showstep=False):
     temp_file_path = str(os.path.join(os.path.join(settings.BASE_DIR, 'temp'), temp_file_name))
     with open(temp_file_path, 'w') as temp_file:
         temp_file.write(sha.hexdigest())
-    return {'string_text': sha.hexdigest(), 'files': {'Hash File': temp_file_path}}, sha.steps
+    return {'string_text': sha.hexdigest(), 'file': {'Hash File': temp_file_path}}, sha.steps
 
 def ecies_visual(msg: str, is_file: bool=False, showstep: bool=False):
     return to_string(ecies(msg, is_filepath=is_file))
